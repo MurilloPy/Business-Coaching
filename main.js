561bd92689091b929ec112a015012cb61991f8d1
MurilloPy <MurilloServico@gmail.com>
2023-08-17 19:04:50 -0300

    0.3

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu");
+const fecharMenu = document.querySelector(".x");
 
 hamburger.addEventListener("click", () => {
 
@@ -7,3 +8,23 @@ hamburger.addEventListener("click", () => {
     menu.classList.toggle("show-menu");
 
 });
+
+document.addEventListener("click", (event) => {
+
+    const targetElement = event.target;
+
+    if (!hamburger.contains(targetElement) && !menu.contains(targetElement)){
+
+        hamburger.classList.remove("open");
+        menu.classList.remove("show-menu");
+
+    };
+
+});
+
+fecharMenu.addEventListener("click", () => {
+
+    fecharMenu.classList.toggle("open");
+    menu.classList.toggle("show-menu");
+
+})
